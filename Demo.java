7ee898d40d2972e853647384db67539ad3557a61
Samip Chaudhary <csamip53@gmail.com>
2026-05-20 21:58:49 +0545

    My First Java Program using variable

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String[] args) {
+//This is my first program in Java Program
+             int age = 25;
+             double price = 19.99;
+             System.out.println("My year is " + age);
+             System.out.println("$" + price);
+           
+    }
+}
