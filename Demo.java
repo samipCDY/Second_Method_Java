022f6535dbd4936bf535e3106a2a387abb4ee53c
Samip Chaudhary <csamip53@gmail.com>
2026-05-21 22:54:53 +0545

    left over variable : string, boolean,if and esle.

--- Demo.java
+++ Demo.java
@@ -1,10 +1,25 @@
 public class Demo {
     public static void main(String[] args) {
 //This is my first program in Java Program
-             int age = 25;
-             double price = 19.99;
-             System.out.println("My year is " + age);
-             System.out.println("$" + price);
+            //  int age = 25;
+            //  double price = 19.99;
+            //  char alphabet = 'A';
+            //  char symbol = '!';
+            //  boolean isstudent =true;
+            //  boolean saleFor =false;
+            //  System.out.println("My year is " + age);
+            //  System.out.println("$" + price);
+            // System.out.println(isstudent);
+            //  System.out.println(alphabet);
+            //  System.out.println(saleFor);
+            // if( isstudent == true){
+            //     //System.out.println("I am god");
+            // }
+            String name = "Samip chaudhary";
+            String name1 = "Tokadone";
+            System.out.println("i am god " + name);
+            System.out.println(name1);
+            System.out.println("your name  " + name + " is very good. " +  "And your nickname is " + name1 );
            
     }
 }
