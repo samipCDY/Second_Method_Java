597d750bfec581fbb93744678041bc659ff0c47c
Samip Chaudhary <csamip53@gmail.com>
2026-05-22 09:15:50 +0545

    #2

--- Demo.java
+++ Demo.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
 public class Demo{
     public static void main(String[] argus){
-        Scanner scanncer = new Scanner(System.in);
-        System.out.println("Enter your name :");
-        String name = scanncer.nextLine();
-        System.out.print("Emter your age: ");
-        int age =  scanncer.nextInt();
-        
-        System.out.println("hello " + name);
-        System.out.println("Your age is " + age + " years old");
+        Scanner scanner = new Scanner(System.in); 
+        // System.out.print("Enter thhe Number of a:");
+        // int a = scanner.nextInt();
+        // System.out.print("Enter the number of b:");
+        // int b = scanner.nextInt();  
+        // if(a>b){
+        //     System.out.println("a is greater than b");
+        // }else if(b>a)
+        //     {
+        //         System.out.println("b is grater than a ");
+        //     }else{
+        //         System.out.println("They are both Equal");
+                
+        //     }    
+        System.out.println("Are you Student ? (true/false) :");
+        boolean student = scanner.nextBoolean();
+        System.out.println("you are a student :" + student);
+        if(student){
+            System.out.println("he is a topper");
+        }
+        else{
+            System.out.println("He is God ");
+        }
 
 
 
-        scanncer.close();
+        scanner.close();
 
     }
 }
