c69679c569e2564548668e1d4125a3b2c811f0ae
Samip Chaudhary <csamip53@gmail.com>
2026-05-23 22:01:26 +0545

    Arithmetic operators

---
+++ Art.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Art {
+    public static void main(String[] argus){
+        // Arithmetic operation
+        Scanner scanner = new Scanner(System.in);
+        int x= 10;
+        int y=5;
+        int z;
+
+        //z=x*y;
+        //z=x+y;
+        //z=x-y;
+        //z=x/y;
+        z=x%y;
+
+        System.out.println(z);
+
+
+        scanner.close();
+    }
+    
+}
