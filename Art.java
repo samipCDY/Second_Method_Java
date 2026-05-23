91a0d5676f74ddfe16e73a6eb4e4efa67fd248f9
Samip Chaudhary <csamip53@gmail.com>
2026-05-23 22:06:54 +0545

    Agumented Operators

--- Art.java
+++ Art.java
@@ -6,16 +6,22 @@ public class Art {
         Scanner scanner = new Scanner(System.in);
         int x= 10;
         int y=5;
-        int z;
+        // int z;
 
-        //z=x*y;
-        //z=x+y;
-        //z=x-y;
-        //z=x/y;
-        z=x%y;
+        // z=x*y;
+        // z=x+y;
+        // z=x-y;
+        // z=x/y;
+        // z=x%y;
 
-        System.out.println(z);
-
+        //System.out.println(z);
+        //Agumented Assignment operator
+        //x +=y;
+        //x -=y;
+        //x *=y; 
+        //x /=y;
+        x %=y;
+        System.out.println(x);
 
         scanner.close();
     }
