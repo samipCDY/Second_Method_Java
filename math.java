572a1999728d6ba22091e63841eac6c6383cc146
Samip Chaudhary <csamip53@gmail.com>
2026-05-26 21:51:56 +0545

    Math Operator using Java

---
+++ math.java
@@ -0,0 +1,23 @@
+public class math {
+    public static void main(String[] argus){
+        // System.out.println(Math.PI);
+        // System.out.println(Math.E);
+
+        double result;
+        // result =Math.pow(2,3);
+        // System.out.println(result);
+        // result = Math.abs(-10);
+        result = Math.sqrt(81);
+        // result = Math.round(2.56);
+        //  result = Math.ceil(3.14);
+        //  result = Math.floor(10.31);
+        
+        System.out.println(result);
+    //  System.out.println(result);
+        // System.out.println(result);
+        // System.out.println(result);
+        // System.out.println(result);
+
+    }
+    
+}
