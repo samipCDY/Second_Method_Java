86d3ace337e15047e48f0cc5f77f2329ec750db9
Samip Chaudhary <csamip53@gmail.com>
2026-05-26 21:33:17 +0545

    Boolean used

--- rand.java
+++ rand.java
@@ -1,16 +1,18 @@
 import java.util.Random;
 public class rand{
     public static void main(String[] argus){
+        //Random Number generator
         Random random = new Random();
-        int number1;
-        int number2;
-        int number3;
-          number1 = random.nextInt(1,6);
-          number2 = random.nextInt(1,6);
-          number3 = random.nextInt(1,6);
-        System.out.print(number1);
-         System.out.print(number2);
-          System.out.print(number3);
+        boolean isHeads;
+        isHeads = random.nextBoolean();
+
+        
+        if(isHeads){
+            System.out.println("It's Heads!");
+        }else{
+            System.out.println("It is Tails!");
+        }
+    
       
 
     }
