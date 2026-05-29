b144600d3916102b61406352cf89dbf5a2203319
Samip Chaudhary <csamip53@gmail.com>
2026-05-29 21:49:32 +0545

    Printf in java

---
+++ printf.java
@@ -0,0 +1,12 @@
+public class printf {
+    public static void main(String[] argus){
+        String name = "Samip";
+        char firstletter = 'S';
+        int age = 22;
+        double height = 5.9;
+        boolean isStudent = true;
+
+        System.out.printf("Hello %s", name);
+    }
+    
+}
